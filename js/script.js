b2157ad825a4973ea28a5e3b6e91d9a4aaed7666
Daniyar <D4nev@mail.ru>
2023-06-20 01:06:06 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+// alert('Hello!')
+//const result = confirm('Are you here?');
+//const answer = prompt('Вам есть 18 лет?', "da");
+const answers = [];
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Какая ваше фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "")
+
+document.write(answers)
